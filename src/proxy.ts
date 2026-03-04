6e5eac8f884406b50e52ee19e391c509770d73cd
antonnif-dev <antonnidev@gmail.com>
2026-03-04 14:33:28 -0300

    reiniciar vercel

--- src/proxy.ts
+++ src/proxy.ts
@@ -12,28 +12,22 @@ export default function proxy(req: NextRequest) {
   const domain = hostname.split(":")[0];
   const pathname = url.pathname;
 
-  // Já está dentro de /sites
   if (pathname.startsWith("/sites/")) {
     return NextResponse.next();
   }
 
-  // acesso local puro
   if (domain === "localhost" || domain === "www.localhost") {
     return NextResponse.next();
   }
 
-  // suporte a clinica.localhost
   if (domain.endsWith(".localhost")) {
     const tenant = domain.replace(".localhost", "");
     return NextResponse.rewrite(
       new URL(`/sites/${tenant}${pathname}`, req.url)
     );
   }
-
-  // 🔥 CASO VERCEL PREFIXADO
+  
   if (domain.endsWith(".vercel.app")) {
-    // exemplo:
-    // clinica-saas-simplao-frontend.vercel.app
 
     const base = "saas-simplao-frontend.vercel.app";
 
